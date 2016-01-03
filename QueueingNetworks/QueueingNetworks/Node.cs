d2bc8c0bb63218a772f17aed48fcf73eedecc473
Jakub Borek <borekjr@gmail.com>
2016-01-03 11:59:31 +0100

    Add GetTransitionProbability method to Node

--- QueueingNetworks/QueueingNetworks/Node.cs
+++ QueueingNetworks/QueueingNetworks/Node.cs
@@ -11,12 +11,24 @@ namespace QueueingNetworks
         public int Id { get; private set; }
         public IReadOnlyList<double> Mi { get; private set; }
         public IReadOnlyList<Connection> Connections { get; private set; }
-
         public Node(int id, IReadOnlyList<double> mi, IReadOnlyList<Connection> connections)
         {
             Id = id;
             Mi = new List<double>(mi);
             Connections = new List<Connection>(connections);
         }  
+
+        public double GetTransitionProbability(int destination, int classId)
+        {
+            var foundConnections = Connections.Where(c => c.Class == classId && c.Destination == destination);
+            if(foundConnections.Any())
+            {
+                return foundConnections.First().Weight;
+            }
+            else
+            {
+                return 0;
+            }
+        }
     }
 }
